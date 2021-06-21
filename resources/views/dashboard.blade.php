16a9f6dd93f569777cb12b4129fc238174a0e46c
Ayuchandra <ayuchandradewi240@gmail.com>
2021-06-22 03:35:06 +0800

    uasfinal

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+
+    <div class="text-center">
+        <h3>SELAMAT DATANG DI GALERI PAKAIAN TARI BALI</h3>
+        <img width="100%" src="{{ ('background.jpeg') }}">
+    </div>
+
+</x-app-layout>
---
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -0,0 +1,3 @@
+<button class="text-black font-bold py-2 px-4">
+    <img src="background.jpeg" alt="logo" style="width:40px;">
+</button>
